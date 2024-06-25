1a1ba8a33d67687dc49bebcd5a21c461fc1ba6cf
Noelia <noeerivaas@gmail.com>
2024-06-25 18:54:33 -0300

    Carga formularios

--- airbnb-app/src/Form/AddAlojamiento.js
+++ airbnb-app/src/Form/AddAlojamiento.js
@@ -1,138 +1,376 @@
-// AdministracionForm.js
-import React, { useState } from 'react';
-import '../styleForms.css/styleForms.css';
+import React, { useState, useEffect } from 'react';
+import '../estilosForms.css/styleForms.css';
 
 const AddAlojamiento = () => {
   const [formData, setFormData] = useState({
+    idAlojamiento: null,
     titulo: '',
     descripcion: '',
     tipoAlojamiento: '',
     latitud: '',
     longitud: '',
     precioPorDia: '',
-    dormitorios: '',
-    baños: '',
+    cantidadDormitorios: '',
+    cantidadBanios: '',
     estado: ''
   });
 
-  const [editIndex, setEditIndex] = useState(null);
-  const [data, setData] = useState([]);
+  const [tiposAlojamiento, setTiposAlojamiento] = useState([]);
+  const [alojamientos, setAlojamientos] = useState([]);
+
+  useEffect(() => {
+    fetchTiposAlojamiento();
+    fetchAlojamientos();
+  }, []);
+
+  const fetchTiposAlojamiento = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/tiposAlojamiento/getTiposAlojamiento');
+      if (response.ok) {
+        const data = await response.json();
+        setTiposAlojamiento(data);
+      } else {
+        console.error('Error al obtener tipos de alojamiento:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error en la conexión:', error);
+    }
+  };
+
+  const fetchAlojamientos = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/alojamiento/getAlojamientos');
+      if (response.ok) {
+        const data = await response.json();
+        setAlojamientos(data);      // Establece los alojamientos obtenidos del backend
+      } else {
+        console.error('Error al obtener alojamientos:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error en la conexión:', error);
+    }
+  };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    });
+    setFormData({ ...formData, [name]: value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const isEmptyField = Object.values(formData).some((value) => !value);
+    const isEmptyField = Object.values(formData).some(value => value === '');
     if (isEmptyField) {
       alert('Por favor, complete todos los campos del formulario.');
       return;
     }
 
-    if (editIndex !== null) {
-      const updatedData = data.map((item, index) => (index === editIndex ? formData : item));
-      setData(updatedData);
-      setEditIndex(null);
-    } else {
-      setData([...data, formData]);
+    const latitud = parseFloat(formData.latitud);
+    const longitud = parseFloat(formData.longitud);
+    const precioPorDia = parseFloat(formData.precioPorDia);
+    const cantidadDormitorios = parseInt(formData.cantidadDormitorios);
+    const cantidadBanios = parseInt(formData.cantidadBanios);
+    const tipoAlojamiento = parseInt(formData.tipoAlojamiento);
+
+    if (isNaN(latitud) || latitud < -90 || latitud > 90) {
+      alert('Por favor, ingrese una latitud válida (entre -90 y 90).');
+      return;
     }
 
+    if (isNaN(longitud) || longitud < -180 || longitud > 180) {
+      alert('Por favor, ingrese una longitud válida (entre -180 y 180).');
+      return;
+    }
+
+    if (isNaN(precioPorDia) || precioPorDia < 0) {
+      alert('Por favor, ingrese un precio por día válido.');
+      return;
+    }
+
+    if (isNaN(cantidadDormitorios) || cantidadDormitorios < 0) {
+      alert('Por favor, ingrese una cantidad de dormitorios válida.');
+      return;
+    }
+
+    if (isNaN(cantidadBanios) || cantidadBanios < 0) {
+      alert('Por favor, ingrese una cantidad de baños válida.');
+      return;
+    }
+
+    if (isNaN(tipoAlojamiento)) {
+      alert('Por favor, seleccione un tipo de alojamiento válido.');
+      return;
+    }
+
+    try {
+      const url = formData.idAlojamiento
+        ? `http://localhost:3001/alojamiento/putAlojamiento/${formData.idAlojamiento}`
+        : 'http://localhost:3001/alojamiento/createAlojamiento';
+      const method = formData.idAlojamiento ? 'PUT' : 'POST';
+
+      const response = await fetch(url, {
+        method,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ 
+          ...formData, 
+          latitud, 
+          longitud, 
+          precioPorDia, 
+          cantidadDormitorios, 
+          cantidadBanios,
+          tipoAlojamiento 
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Error en la creación del alojamiento');
+      }
+
+      const newAlojamiento = await response.json();
+      setAlojamientos(formData.idAlojamiento
+        ? alojamientos.map(alojamiento => alojamiento.idAlojamiento === newAlojamiento.idAlojamiento ? newAlojamiento : alojamiento)
+        : [...alojamientos, newAlojamiento]);
+
+      alert(`Alojamiento ${formData.idAlojamiento ? 'actualizado' : 'creado'} creado correctamente.`);
+
+      setFormData({
+        idAlojamiento: null,
+        titulo: '',
+        descripcion: '',
+        tipoAlojamiento: '',
+        latitud: '',
+        longitud: '',
+        precioPorDia: '',
+        cantidadDormitorios: '',
+        cantidadBanios: '',
+        estado: ''
+      });
+    } catch (error) {
+      console.error('Error al enviar los datos:', error);
+      alert('Ocurrió un error al enviar los datos. Por favor, intenta nuevamente más tarde.');
+    }
+  };
+
+  const handleEdit = (alojamiento) => {
     setFormData({
-      titulo: '',
-      descripcion: '',
-      tipoAlojamiento: '',
-      latitud: '',
-      longitud: '',
-      precioPorDia: '',
-      dormitorios: '',
-      baños: '',
-      estado: ''
+      idAlojamiento: alojamiento.idAlojamiento,
+      titulo: alojamiento.Titulo,
+      descripcion: alojamiento.Descripcion,
+      tipoAlojamiento: alojamiento.TipoAlojamiento,
+      latitud: alojamiento.Latitud,
+      longitud: alojamiento.Longitud,
+      precioPorDia: alojamiento.PrecioPorDia,
+      cantidadDormitorios: alojamiento.CantidadDormitorios,
+      cantidadBanios: alojamiento.CantidadBanios,
+      estado: alojamiento.Estado
     });
   };
 
-  const handleEdit = (index) => {
-    setFormData(data[index]);
-    setEditIndex(index);
-  };
 
-  const handleDelete = (index) => {
-    const updatedData = data.filter((_, i) => i !== index);
-    setData(updatedData);
+  const handleDelete = async (id) => {
+    if (window.confirm('¿Estás seguro de eliminar este alojamiento?')) {
+      try {
+        const response = await fetch(`http://localhost:3001/alojamiento/deleteAlojamiento/${id}`, {
+          method: 'DELETE',
+        });
+  
+        if (!response.ok) {
+          throw new Error('Error al eliminar el alojamiento');
+        }
+  
+        // Actualizar la lista de alojamientos después de la eliminación
+        fetchAlojamientos();
+  
+        alert('Alojamiento eliminado correctamente.');
+      } catch (error) {
+        console.error('Error al eliminar el alojamiento:', error);
+        alert('Ocurrió un error al eliminar el alojamiento. Por favor, intenta nuevamente más tarde.');
+      }
+    }
   };
+  
+  
 
   return (
     <>
-      <section className="formulario">
+      <section className="formulario-alojamiento">
         <h2>Registrar Alojamiento</h2>
-        <div className="cuadro">
+        <div className="cuadro-alojamiento">
           <form onSubmit={handleSubmit}>
             <div className="form-row">
-              <div className="form-column">
-                {['titulo', 'tipoAlojamiento', 'latitud', 'precioPorDia', 'dormitorios'].map((key) => (
-                  <div className="form-group" key={key}>
-                    <label className="label" htmlFor={key}>{key.charAt(0).toUpperCase() + key.slice(1)}</label>
-                    {key === 'tipoAlojamiento' ? (
-                      <select className="casilla" id={key} name={key} value={formData[key]} onChange={handleChange} required>
-                        <option value="">Tipo de Alojamiento</option>
-                        <option value="casa">Casa</option>
-                        <option value="departamento">Departamento</option>
-                        <option value="habitacion">Habitación</option>
-                      </select>
-                    ) : (
-                      <input className="casilla" type={key === 'precioPorDia' || key === 'latitud' || key === 'longitud' ? 'text' : key === 'dormitorios' || key === 'baños' ? 'number' : 'text'} id={key} name={key} value={formData[key]} onChange={handleChange} placeholder={key.charAt(0).toUpperCase() + key.slice(1)} required />
-                    )}
-                  </div>
-                ))}
+              <div className="form-group">
+                <label className="label" htmlFor="titulo">Título</label>
+                <input
+                  className="casilla-alojamiento"
+                  type="text"
+                  id="titulo"
+                  name="titulo"
+                  value={formData.titulo}
+                  onChange={handleChange}
+                  placeholder="Ingrese el título del alojamiento"
+                  required
+                />
               </div>
-              <div className="form-column">
-                {['descripcion', 'longitud', 'baños', 'estado'].map((key) => (
-                  <div className="form-group" key={key}>
-                    <label className="label" htmlFor={key}>{key.charAt(0).toUpperCase() + key.slice(1)}</label>
-                    {key === 'descripcion' ? (
-                      <textarea className="casilla" id={key} name={key} value={formData[key]} onChange={handleChange} placeholder={key.charAt(0).toUpperCase() + key.slice(1)} required />
-                    ) : (
-                      <input className="casilla" type={key === 'precioPorDia' || key === 'latitud' || key === 'longitud' ? 'text' : key === 'dormitorios' || key === 'baños' ? 'number' : 'text'} id={key} name={key} value={formData[key]} onChange={handleChange} placeholder={key.charAt(0).toUpperCase() + key.slice(1)} required />
-                    )}
-                  </div>
-                ))}
+              <div className="form-group">
+                <label className="label" htmlFor="descripcion">Descripción</label>
+                <textarea
+                  className="casilla-alojamiento"
+                  id="descripcion"
+                  name="descripcion"
+                  value={formData.descripcion}
+                  onChange={handleChange}
+                  placeholder="Ingrese la descripción del alojamiento"
+                  required
+                />
               </div>
             </div>
-            <div className="boton-container">
-              <button type="submit">{editIndex !== null ? 'Guardar cambios' : 'Registrar'}</button>
+            <div className="form-row">
+              <div className="form-group">
+                <label className="label" htmlFor="tipoAlojamiento">Tipo de Alojamiento</label>
+                <select
+                  className="casilla-alojamiento"
+                  id="tipoAlojamiento"
+                  name="tipoAlojamiento"
+                  value={formData.tipoAlojamiento}
+                  onChange={handleChange}
+                  required
+                >
+                  <option value="">Seleccione el tipo de alojamiento</option>
+                  {tiposAlojamiento.map(tipo => (
+                    <option key={tipo.idTipoAlojamiento} value={tipo.idTipoAlojamiento}>{tipo.Descripcion}</option>
+                  ))}
+                </select>
+              </div>
+              <div className="form-group">
+                <label className="label" htmlFor="latitud">Latitud</label>
+                <input
+                  className="casilla-alojamiento"
+                  type="text"
+                  id="latitud"
+                  name="latitud"
+                  value={formData.latitud}
+                  onChange={handleChange}
+                  placeholder="Ingrese la latitud"
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label className="label" htmlFor="longitud">Longitud</label>
+                <input
+                  className="casilla-alojamiento"
+                  type="text"
+                  id="longitud"
+                  name="longitud"
+                  value={formData.longitud}
+                  onChange={handleChange}
+                  placeholder="Ingrese la longitud"
+                  required
+                />
+              </div>
+            </div>
+            <div className="form-row">
+              <div className="form-group">
+                <label className="label" htmlFor="precioPorDia">Precio por Día</label>
+                <input
+                  className="casilla-alojamiento"
+                  type="text"
+                  id="precioPorDia"
+                  name="precioPorDia"
+                  value={formData.precioPorDia}
+                  onChange={handleChange}
+                  placeholder="Ingrese el precio por día"
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label className="label" htmlFor="cantidadDormitorios">Dormitorios</label>
+                <input
+                  className="casilla-alojamiento"
+                  type="number"
+                  id="cantidadDormitorios"
+                  name="cantidadDormitorios"
+                  value={formData.cantidadDormitorios}
+                  onChange={handleChange}
+                  placeholder="Ingrese la cantidad de dormitorios"
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label className="label" htmlFor="cantidadBanios">Baños</label>
+                <input
+                  className="casilla-alojamiento"
+                  type="number"
+                  id="cantidadBanios"
+                  name="cantidadBanios"
+                  value={formData.cantidadBanios}
+                  onChange={handleChange}
+                  placeholder="Ingrese la cantidad de baños"
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label className="label" htmlFor="estado">Estado</label>
+                <select
+                  className="casilla-alojamiento"
+                  id="estado"
+                  name="estado"
+                  value={formData.estado}
+                  onChange={handleChange}
+                  required
+                >
+                  <option value="">Seleccione el estado del alojamiento</option>
+                  <option value="Disponible">Disponible</option>
+                  <option value="Reservado">Reservado</option>
+                </select>
+              </div>
+            </div>
+            <div className="boton-container-alojamiento">
+            <button type="submit">
+              {formData.idAlojamiento ? 'Guardar Cambios' : 'Registrar Alojamiento'}
+            </button>
             </div>
           </form>
         </div>
       </section>
 
-      <section className="tabla">
+
+      <section className="tabla-alojamiento">
         <div className="contenido-centrado">
-          <div className="tabla-container">
-            <h2>Datos Registrados</h2>
+          <div className="tabla-container-alojamiento">
+            <h2>Alojamientos Registrados</h2>
             <table id="datosTabla">
               <thead>
                 <tr>
-                  {Object.keys(formData).map((key) => (
-                    <th key={key}>{key.charAt(0).toUpperCase() + key.slice(1)}</th>
-                  ))}
+                  <th>Título</th>
+                  <th>Descripción</th>
+                  <th>Tipo Alojamiento</th>
+                  <th>Latitud</th>
+                  <th>Longitud</th>
+                  <th>Precio por Día</th>
+                  <th>Dormitorios</th>
+                  <th>Baños</th>
+                  <th>Estado</th>
                   <th>Acciones</th>
                 </tr>
               </thead>
               <tbody>
-                {data.map((item, index) => (
-                  <tr key={index}>
-                    {Object.values(item).map((value, i) => (
-                      <td key={i}>{value}</td>
-                    ))}
+                {alojamientos.map(alojamiento => (
+                  <tr key={alojamiento.idAlojamiento}>
+                    <td>{alojamiento.Titulo}</td>
+                    <td>{alojamiento.Descripcion}</td>
+                    <td>{alojamiento.TipoAlojamiento}</td>
+                    <td>{alojamiento.Latitud}</td>
+                    <td>{alojamiento.Longitud}</td>
+                    <td>{alojamiento.PrecioPorDia}</td>
+                    <td>{alojamiento.CantidadDormitorios}</td>
+                    <td>{alojamiento.CantidadBanios}</td>
+                    <td>{alojamiento.Estado}</td>
                     <td>
-                      <button className="edit-btn" onClick={() => handleEdit(index)}>Editar</button>
-                      <button className="delete-btn" onClick={() => handleDelete(index)}>Eliminar</button>
-                    </td>
+                      <button className='edit-btn' onClick={() => handleEdit(alojamiento)}>Editar</button>
+                      <button className='delete-btn' onClick={() => handleDelete(alojamiento.idAlojamiento)}>Eliminar</button>
+                      </td>
                   </tr>
                 ))}
               </tbody>
--- airbnb-app/src/components/Administracion.js
+++ airbnb-app/src/components/Administracion.js
@@ -1,138 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import '../styleForms.css/styleForms.css';
+import React from 'react';
+import { Link, Routes, Route, useLocation } from 'react-router-dom';
+import AddTipoAlojamiento from '../Form/AddTipoAlojamiento';
+import AddAlojamiento from '../Form/AddAlojamiento';
+import '../estilosForms.css/styleAdministracion.css';
 
 const Administracion = () => {
-  const [formData, setFormData] = useState({ descripcion: '' });
-  const [editIndex, setEditIndex] = useState(null);
-  const [data, setData] = useState([]);
-  const [idCounter, setIdCounter] = useState(1);
-
-  useEffect(() => {
-    fetch('http://localhost:3001/alojamiento')
-      .then(response => response.json())
-      .then(data => setData(data))
-      .catch(error => console.error('Error al obtener datos:', error));
-  }, []);
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    if (!formData.descripcion) {
-      alert('Por favor, ingrese una descripción.');
-      return;
-    }
-
-    if (editIndex !== null) {
-      const updatedData = { ...data[editIndex], ...formData };
-
-      // Enviar solicitud PUT para actualizar tipos de alojamientos exitentes
-      await fetch(`http://localhost:3001/tiposAlojamiento/putTipoAlojamiento/:id/${updatedData.id}`, {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(updatedData),
-      });
-
-      const newData = data.map((item, index) => (index === editIndex ? updatedData : item));
-      setData(newData);
-      setEditIndex(null);
-    } else {
-      const newData = { id: idCounter, ...formData };
-
-      // Enviar solicitud POST para crear un nuevo  alojamiento
-      await fetch('http://localhost:3001/tiposAlojamiento/createTipoAlojamiento', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(newData),
-      });
-
-      setData([...data, newData]);
-      setIdCounter(idCounter + 1);
-    }
-
-    setFormData({ descripcion: '' });
-  };
-
-  const handleEdit = (index) => {
-    setFormData(data[index]);
-    setEditIndex(index);
-  };
-
-  const handleDelete = async (index) => {
-    const itemToDelete = data[index];
-
-    // Enciar solicitud DELETE para eliminar tipo de alojamiento
-    await fetch(`http://localhost:3001/tiposAlojamiento/deleteTipoAlojamiento/:id/${itemToDelete.id}`, {
-      method: 'DELETE',
-    });
-
-    const updatedData = data.filter((_, i) => i !== index);
-    setData(updatedData);
-    if (editIndex === index) {
-      setFormData({ descripcion: '' });
-      setEditIndex(null);
-    }
-  };
+  const location = useLocation();
 
   return (
     <>
-      <section className="formulario-alojamiento">
-        <h2>Nuevo Tipo de Alojamiento</h2>
-        <div className="cuadro-alojamiento">
-          <form onSubmit={handleSubmit}>
-            <div className="form-row">
-              <div className="form-group">
-                <label className="label" htmlFor="descripcion">Descripción</label>
-                <input
-                  className="casilla-alojamiento"
-                  id="descripcion"
-                  name="descripcion"
-                  value={formData.descripcion}
-                  onChange={handleChange}
-                  placeholder="Ingrese su nuevo tipo de alojamiento"
-                  required
-                />
-              </div>
-            </div>
-            <div className="boton-container-alojamiento">
-              <button type="submit">{editIndex !== null ? 'Guardar cambios' : 'Registrar'}</button>
-            </div>
-          </form>
-        </div>
-      </section>
+      <div>
+        <h2 className='titulos-h2'>Administración del sitio</h2>
+        <ul className='mini-nav'>
+          <li className='nav-admin'>
+            <Link to="./addTipoAlojamiento" className={location.pathname === '/addTipoAlojamiento' ? 'nav-link-form activo' : 'nav-link-form'}>Nuevo Tipo Alojamiento</Link>
+          </li>
+          <li className='nav-admin'>
+            <Link to="./AddAlojamiento" className={location.pathname === '/AddAlojamiento' ? 'nav-link-form activo' : 'nav-link-form'}>Nuevo Alojamiento</Link>
+          </li>
+        </ul>
+      </div>
 
-      <section className="tabla-alojamiento">
-        <div className="contenido-centrado">
-          <div className="tabla-container-alojamiento">
-            <h2>Alojamientos Registrados</h2>
-            <table id="datosTabla">
-              <thead>
-                <tr>
-                  <th>ID</th>
-                  <th>Descripción</th>
-                  <th>Acciones</th>
-                </tr>
-              </thead>
-              <tbody>
-                {data.map((item, index) => (
-                  <tr key={index}>
-                    <td>{item.id}</td>
-                    <td>{item.descripcion}</td>
-                    <td>
-                      <button className="edit-btn" onClick={() => handleEdit(index)}>Editar</button>
-                      <button className="delete-btn" onClick={() => handleDelete(index)}>Eliminar</button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </section>
+      <Routes>
+        <Route path="/addTipoAlojamiento" element={<AddTipoAlojamiento />} />
+        <Route path="/AddAlojamiento" element={<AddAlojamiento />} />
+      </Routes>
     </>
   );
 };
